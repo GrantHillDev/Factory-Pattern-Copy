2302331e46d6d9f01dddeed444ea93eb82876526
Grant Hill <granthill@live.com>
2025-04-03 18:45:04 -0400

    mild updates.

--- FactoryPattern/ConsoleLogger.cs
+++ FactoryPattern/ConsoleLogger.cs
@@ -10,7 +10,7 @@ namespace FactoryPattern
     {
         public static void VehicleStartUp()//so, why not just make this a part of the IVehicle interface? Particularly if it's going to be implemented through each derived vehicle class? I need more of a visualization to see how much memory a static method such as this is actually saving, versus just a stubbed out method, or a regularly typed method.
         {
-            Console.WriteLine("Sounds like there was click of the ignition....");
+            Console.WriteLine("Sounds like there was a click of the ignition....");
             Console.WriteLine();
             Thread.Sleep(1000);
             Console.WriteLine("Oh, and there goes the engine!");
